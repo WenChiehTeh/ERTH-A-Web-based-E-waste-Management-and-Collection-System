6f0a0e43d198f3982b1dc1c831e32645e3beca54
TEH WEN CHIEH <wenchiehteh@gmail.com>
2025-04-15 09:08:46 +0800

    add DML

---
+++ dataManipulationLanguage(DML).sql
@@ -0,0 +1,13 @@
+--admin values
+INSERT INTO admins(name, username, password, role) VALUES 
+( "TEH WEN CHIEH", "TWC", "$2b$10$Nvr.rCF./FG8i5vBA5MnPOZFtw3dU90aGaeviorjR6xbV918tLOdy", "Admin"),
+( "TENG YIK SHEN", "TYS", "$2b$10$ibNhaZ3oiLhwqDLzVuRoWecKZ64wrmJkK9vYSENDT.ogHenXEXoja", "Warehouse Manager"),
+( "YAP KEAN JIE", "YKJ", "$2b$10$/xpjTfnCch0AKmgBofYknOWmEt2hEp0ujVAnkXsGTtrVDwmU1a9NC", "Driver"),
+( "TAN LI XUAN", "TLX", "$2b$10$IWXALKxlS.cuU46zDe6l0eabhU8MVB8RjUfVzlTLZdkS7vUXM3wXS", "Driverr"),
+( "TEOPHILUS ROBER", "TPR", "$2b$10$wp2K89sSRzU1sHHOFJ8Meez6Gcv85133jlv3xtU4.4ZE56btoqlKC", "Warehouse Manager"),
+( "THEN WEI CHENG", "WCT", "$2b$10$5DcV/sr3nXJiZ5ccQgZnQOWtkD3rfIANj07BqcF3.xm91KTxOc8fW", "ADMIN")
+;
+
+INSERT INTO driver(vehicletype, numberplate, driverid) VALUES
+("Lorry", "ABC1234", "3"),
+("truck", "ABC4321", "4");
